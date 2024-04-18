e11043ccc0c194bbc22d04c85fd3218c2b561f8d
TheProgxy <theprogxy@gmail.com>
2024-04-18 22:27:01 +0200

    fix

--- ml.c
+++ ml.c
@@ -24,8 +24,8 @@ int main() {
     unsigned int arch[] = {2, 2, 1};
     Ml ml = create_ml(ARR_SIZE(arch), arch);
     rand_ml(ml);
-    Mat input_mat = (Mat) {.rows = 4, .cols = 2, .data = (double*) input_data};
-    Mat output_mat = (Mat) {.rows = 4, .cols = 1, .data = (double*) output_data};
+    Mat input_mat = (Mat) { .rows = 4, .cols = 2, .data = (double*) input_data };
+    Mat output_mat = (Mat) { .rows = 4, .cols = 1, .data = (double*) output_data };
     learn(ml, input_mat, output_mat, 1e-3, 1000);
     double cost_ml = cost(ml, input_mat, output_mat);
     printf("Current cost: %lf\n", cost_ml);
