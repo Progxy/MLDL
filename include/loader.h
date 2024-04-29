4d7424a79c603a05791257bcb74ac641fb1b2fa1
TheProgxy <theprogxy@gmail.com>
2024-04-29 21:25:33 +0200

    fix

--- include/loader.h
+++ include/loader.h
@@ -15,4 +15,32 @@ void read_file(File* file_data) {
     return;
 }
 
+char** split(char* str, char splitter, unsigned int* lines_count) {
+    char** lines = (char**) calloc(1, sizeof(char*));
+    char* line = (char*) calloc(1, sizeof(char));
+    *lines_count = 0;
+    unsigned int line_len = 0;
+    for (unsigned int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == splitter) {
+            lines = (char**) realloc(lines, sizeof(char*) * (*lines_count + 1));
+            lines[(*lines_count)++] = line;
+            line = (char*) calloc(1, sizeof(char));
+            line_len = 0;
+            continue;
+        }
+        line = (char*) realloc(line, sizeof(char) * (line_len + 1));
+        line[line_len++] = str[i];
+    }
+    if (line[0] != '\0') {
+        lines = (char**) realloc(lines, sizeof(char*) * (*lines_count + 1));
+        lines[(*lines_count)++] = line;
+    } else free(line);
+    return lines;
+}
+
+void parse_dataset(File* dataset, Tensor* inputs, Tensor* outputs) {
+    
+    return;
+}
+
 #endif //_LOADER_H_
