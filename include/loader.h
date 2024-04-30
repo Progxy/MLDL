b3c51c8ba5e9530eede5ee9d4040fbc249d9b57a
TheProgxy <theprogxy@gmail.com>
2024-04-30 11:55:54 +0200

    fix

--- include/loader.h
+++ include/loader.h
@@ -3,7 +3,11 @@
 
 #include "./utils.h"
 
-void read_file(File* file_data) {
+void parse_dataset(File* dataset, Tensor* inputs, unsigned int input_size, Tensor* outputs, unsigned int output_size);
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+static void read_file(File* file_data) {
     FILE* file = fopen(file_data -> file_name, "rb");
     ASSERT(file == NULL, "OPEN_FILE_ERROR");
     fseek(file, 0, SEEK_END);
@@ -11,11 +15,11 @@ void read_file(File* file_data) {
     fseek(file, 0, SEEK_SET);
     file_data -> data = (unsigned char*) calloc(file_data -> size, sizeof(unsigned char));
     int read_size = fread(file_data -> data, sizeof(unsigned char), file_data -> size, file);
-    ASSERT((read_size != file_data -> size) || ferror(file), "READ_ERROR");
+    ASSERT((read_size != (int) file_data -> size) || ferror(file), "READ_ERROR");
     return;
 }
 
-char** split(char* str, char splitter, unsigned int* lines_count) {
+static char** split(char* str, char splitter, unsigned int* lines_count) {
     char** lines = (char**) calloc(1, sizeof(char*));
     char* line = (char*) calloc(1, sizeof(char));
     *lines_count = 0;
@@ -38,22 +42,26 @@ char** split(char* str, char splitter, unsigned int* lines_count) {
     return lines;
 }
 
-unsigned int str_len(char* str) {
+static unsigned int str_len(char* str) {
     unsigned int len = 0;
     if (str == NULL) return len;
     for (unsigned int i = 0; str[i] != '\0'; ++i, ++len);
     return len;
 }
 
-bool str_cmp(char* a, char* b) {
+static bool str_cmp(char* a, char* b) {
     if (str_len(a) != str_len(b)) return FALSE;
     for (unsigned int i = 0; str_len(a); ++i) if (a[i] != b[i]) return FALSE;
     return TRUE;
 }
 
 void parse_dataset(File* dataset, Tensor* inputs, unsigned int input_size, Tensor* outputs, unsigned int output_size) {
+    read_file(dataset);
+
+    printf("DEBUG_INFO: parsing the dataset...\n");
     unsigned int lines_count = 0;
-    char** dataset_lines = split(dataset -> data, '\n', &lines_count);
+    char** dataset_lines = split(CAST_PTR(dataset -> data, char), '\n', &lines_count);
+
     void* input_data = calloc(input_size * lines_count, inputs -> data_type);
     void* output_data = calloc(output_size * lines_count, outputs -> data_type);
     unsigned int input_data_size = 0;
@@ -64,15 +72,15 @@ void parse_dataset(File* dataset, Tensor* inputs, unsigned int input_size, Tenso
         char** dataset_line = split(dataset_lines[i], ',', &line_count);
         for (unsigned int j = 0; j < line_count; ++j) {
             if (j < input_size) {
-                if (inputs -> data_type == FLOAT_32) CAST_PTR(input_data, float)[input_data_size++] = str_cmp(dataset_line[i], "x") ? 2 : str_cmp(dataset_line[i], "o"); 
-                else if (inputs -> data_type == FLOAT_64) CAST_PTR(input_data, double)[input_data_size++] = str_cmp(dataset_line[i], "x") ? 2 : str_cmp(dataset_line[i], "o"); 
-                else if (inputs -> data_type == FLOAT_128) CAST_PTR(input_data, long double)[input_data_size++] = str_cmp(dataset_line[i], "x") ? 2 : str_cmp(dataset_line[i], "o"); 
+                if (inputs -> data_type == FLOAT_32) CAST_PTR(input_data, float)[input_data_size++] = str_cmp(dataset_line[j], "x") ? 2.0f : (float) str_cmp(dataset_line[j], "o"); 
+                else if (inputs -> data_type == FLOAT_64) CAST_PTR(input_data, double)[input_data_size++] = str_cmp(dataset_line[j], "x") ? 2.0 : (double) str_cmp(dataset_line[j], "o"); 
+                else if (inputs -> data_type == FLOAT_128) CAST_PTR(input_data, long double)[input_data_size++] = str_cmp(dataset_line[j], "x") ? 2.0L : (long double) str_cmp(dataset_line[j], "o"); 
             } else {
-                if (outputs -> data_type == FLOAT_32) CAST_PTR(output_data, float)[output_data_size++] = str_cmp(dataset_line[i], "positive"); 
-                else if (outputs -> data_type == FLOAT_64) CAST_PTR(output_data, double)[output_data_size++] = str_cmp(dataset_line[i], "positive"); 
-                else if (outputs -> data_type == FLOAT_128) CAST_PTR(output_data, long double)[output_data_size++] = str_cmp(dataset_line[i], "positive"); 
+                if (outputs -> data_type == FLOAT_32) CAST_PTR(output_data, float)[output_data_size++] = (float) str_cmp(dataset_line[j], "positive"); 
+                else if (outputs -> data_type == FLOAT_64) CAST_PTR(output_data, double)[output_data_size++] = (double) str_cmp(dataset_line[j], "positive"); 
+                else if (outputs -> data_type == FLOAT_128) CAST_PTR(output_data, long double)[output_data_size++] = (long double) str_cmp(dataset_line[j], "positive"); 
             }
-            DEALLOCATE_PTRS(dataset_line[i]);
+            DEALLOCATE_PTRS(dataset_line[j]);
         }
         DEALLOCATE_PTRS(dataset_lines[i], dataset_line);
     }
@@ -81,10 +89,11 @@ void parse_dataset(File* dataset, Tensor* inputs, unsigned int input_size, Tenso
 
     unsigned int input_shape[] = { lines_count, input_size };
     unsigned int output_shape[] = { lines_count, output_size };
-    reshape_tensor(inputs, input_shape, ARR_SIZE(input_shape), inputs -> data_type);
-    reshape_tensor(outputs, output_shape, ARR_SIZE(output_shape), outputs -> data_type);
-    set_tensor(input_data, *inputs);
-    set_tensor(input_data, *outputs);
+    set_tensor(input_data, *reshape_tensor(inputs, input_shape, ARR_SIZE(input_shape), inputs -> data_type));
+    set_tensor(input_data, *reshape_tensor(outputs, output_shape, ARR_SIZE(output_shape), outputs -> data_type));
+    DEALLOCATE_PTRS(input_data, output_data);
+
+    printf("DEBUG_INFO: dataset successfully parsed.\n");
 
     return;
 }
--- ml.c
+++ ml.c
@@ -5,53 +5,39 @@
 #include "./include/loader.h"
 
 int main() {
-    init_seed();
-    
-    const double input_data[] = {
-        1.0, 1.0,
-        0.0, 1.0,
-        1.0, 0.0,
-        0.0, 0.0
-    };    
-    
-    const double output_data[] = {
-        1.0, 
-        0.0, 
-        0.0,
-        0.0,
-    };
+    //init_seed();
 
-    unsigned int arch[] = {2, 4, 1};
+    unsigned int arch[] = {9, 7, 7, 1};
     NN nn = create_nn(ARR_SIZE(arch), arch, FLOAT_64);
     rand_nn(nn);
 
-    unsigned int input_shape[] = {4, 2};
-    Tensor input = alloc_tensor(input_shape, ARR_SIZE(input_shape), nn.data_type);
-    set_tensor((void*) input_data, input);
-    unsigned int output_shape[] = {4, 1};
-    Tensor output = alloc_tensor(output_shape, ARR_SIZE(output_shape), nn.data_type);
-    set_tensor((void*) output_data, output);
+    Tensor inputs = empty_tensor(nn.data_type);
+    Tensor outputs = empty_tensor(nn.data_type);
+    File dataset = (File) { .file_name = "././datasets/tic_tac_toe_ds.arff", .data = NULL, .size = 0 };
+    parse_dataset(&dataset, &inputs, nn.arch[0], &outputs, nn.arch[nn.size - 1]);
 
     double alpha = 0.01;
     double cost_d = 0.0;
-    unsigned int max_epochs = 1000;
+    unsigned int max_epochs = 10;
     double eps = 10e-8;
     double first_moment_decay = 0.9;
     double second_moment_decay = 0.999;
-    adam_optim(nn, input, output, &alpha, &eps, &first_moment_decay, &second_moment_decay, max_epochs);
+    print_nn(nn);
+    adam_optim(nn, inputs, outputs, &alpha, &eps, &first_moment_decay, &second_moment_decay, max_epochs);
+    print_nn(nn);
     //sgd(nn, input, output, &alpha, max_epochs);
-    printf("NN accuracy: %.2lf%%\n", (1.0 - *CAST_PTR(cost(nn, input, output, &cost_d), double)) * 100.0);
-    DEALLOCATE_TENSORS(input, output);
+    printf("NN accuracy: %.2lf%%\n", (1.0 - *CAST_PTR(cost(nn, inputs, outputs, &cost_d), double)) * 100.0);
+    DEALLOCATE_TENSORS(inputs, outputs);
     
-    const double predict_input[] = {
-        0.0, 1.0
-    };
-    unsigned int predict_shape[] = {1, 2};
+    const double predict_input[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+    unsigned int predict_shape[] = {1, nn.arch[0]};
     Tensor input_tensor = alloc_tensor(predict_shape, 2, nn.data_type);
     set_tensor((void*) predict_input, input_tensor);
     Tensor output_tensor = empty_tensor(nn.data_type);
     predict(nn, input_tensor, &output_tensor);
-    PRINT_TENSOR(output_tensor);
+    printf("input: ");
+    for (unsigned char i = 0; i < 9; ++i) printf("'%c'%c", predict_input[i] == 0.0 ? 'b' : predict_input[i] == 1.0 ? 'x' : 'o', i == 8 ? '\n' : ',');
+    printf("result: %.2lf%% 'positive'\n", CAST_PTR(output_tensor.data, double)[0]);
     DEALLOCATE_TENSORS(input_tensor, output_tensor);
 
     deallocate_nn(nn);
